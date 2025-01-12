0f39c906287a4ba3f5092ec599f95f7fb42ba519
Bruno Salgado <bsalgado@stevens.edu>
2025-01-12 14:46:11 -0500

    feat: add toggleable items with 5 min reset countdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,34 @@
 import ReminderItem from "@/components/ReminderItem/ReminderItem";
 import styles from "./page.module.css"
 import { Tile } from "@carbon/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
-  const [currentTime, setTime] = useState(new Date().toLocaleTimeString())
+  const RESET_SIGNAL_COUNTDOWN = 300000
+  let time = new Date().toLocaleTimeString()
+  const [currentTime, setTime] = useState(time)
+  const [resetSignal, setResetSignal] = useState(0)
   
   const updateTime = () => {
-    const time = new Date().toLocaleTimeString()
+    time = new Date().toLocaleTimeString()
     setTime(time)
   }
-  setInterval(updateTime)
+  
+  useEffect(() => {
+    setInterval(updateTime)
+  }, [])
+
+  const handleTouch = (event: React.MouseEvent<HTMLElement>) => {
+    console.log(`[TOUCH DETECTED - STARTING ${RESET_SIGNAL_COUNTDOWN / 60000} MIN COUNTDOWN]`)
+    setTimeout(() => {
+      console.log('[RESETTING...]')
+      setResetSignal(resetSignal => resetSignal + 1)
+    }, RESET_SIGNAL_COUNTDOWN)
+    // TODO: How to restart the reset signal countdown if a new click comes in before the countdown is up?
+    // e.g. 1 item is clicked -> 30s later, another item is clicked -> How to reset the countdown so that
+    // they all get reset 5 min after the second one was clicked ?
+  }
 
   const items: Item[] = [
     {
@@ -45,11 +62,13 @@ export default function Home() {
     }
   ]
   return (
-    // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <div className={styles.root}>
+    <div className={styles.root} onClick={handleTouch}>
+      <header className={styles.rootHeader}>
+        <h1 className={styles.headerTitle}>Things I Need Always</h1>
+      </header>
       <Tile className={styles.itemsContainer}>
         {
-          items.map(({ name, resource }, idx) => <ReminderItem name={name} resource={resource} key={idx}></ReminderItem>)
+          items.map(({ name, resource }, idx) => <ReminderItem name={name} resource={resource} resetSignal={resetSignal} key={idx}></ReminderItem>)
         }
       </Tile>
       <footer className={styles.rootFooter}>
--- src/components/ReminderItem/ReminderItem.tsx
+++ src/components/ReminderItem/ReminderItem.tsx
@@ -1,14 +1,27 @@
+'use client'
 import { Tile } from "@carbon/react"
 import styles from "./ReminderItem.module.css"
 import { NextPage } from "next"
+import { useEffect, useState } from "react"
 
 interface ReminderItemType {
   name: string,
-  resource: string
+  resource: string,
+  resetSignal: number
 }
-const ReminderItem: NextPage<ReminderItemType> = ({ name, resource }) => {
+const ReminderItem: NextPage<ReminderItemType> = ({ name, resource, resetSignal }) => {
+  const [toggled, setToggled] = useState(false)
+
+  const handleTileClick = (event: React.MouseEvent<HTMLElement>) => {
+    setToggled(!toggled)
+  }
+
+  useEffect(() => {
+    setToggled(false)
+  }, [resetSignal])
+
   return (
-    <Tile className={styles.root}>
+    <Tile className={`${styles.root} ${toggled ? styles.toggled : styles.untoggled}`} onClick={handleTileClick}>
       <div className={styles.resourceContainer}>
         {resource}
       </div>
